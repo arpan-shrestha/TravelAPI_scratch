28c38f01e593ee4a3a801d2c9da12ba5023ad1d3
Arpan Shrestha <arpanshrestha12@gmail.com>
2025-08-21 12:04:09 +0545

    Added Fetch method

---
+++ scratchAPI/main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('domestic-destinations/', views.fetch_domestic_trips, name='fetch_domestic_trips'),
+    path('international-destinations/',views.fetch_international_trips,name='fetch_international_trips')
+]
--- scratchAPI/main/views.py
+++ scratchAPI/main/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import DomesticTrip
+from django.http import JsonResponse
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+def fetch_domestic_trips(request):
+    url = "https://www.antholidays.com/destination-domestic/"
+    domestic_destinations=[]
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        items = soup.select('div.hover\\:shadow-lg.border')
+
+        for item in items:
+            title = item.find('h2').text.strip() if item.find('h2') else 'No Title'
+            description = item.find('p').text.strip() if item.find('p') else 'No Description'
+
+            domestic_destinations.append({
+                'title':title,
+                'description':description
+            })
+    return JsonResponse({"domestic_destination": domestic_destinations})
+
+def fetch_international_trips(request):
+    url = "https://www.antholidays.com/destination-international"
+    international_destinations=[]
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        items = soup.select('div.hover\\:shadow-lg.border')
+
+        for item in items:
+            title = item.find('h2').text.strip() if item.find('h2') else 'No Title'
+            description = item.find('p').text.strip() if item.find('p') else 'No Description'
+
+            international_destinations.append({
+                'title':title,
+                'description':description
+            })
+    return JsonResponse({"international_destination": international_destinations})
