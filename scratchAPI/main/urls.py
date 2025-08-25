40604c39572ef4b6fa5b2f8ae9c6f81dff30ffa2
Arpan Shrestha <arpanshrestha12@gmail.com>
2025-08-25 15:30:20 +0545

    ADDED CRUD

--- scratchAPI/main/urls.py
+++ scratchAPI/main/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('international-destinations/',views.fetch_international_trips,name='fetch_international_trips'),
     path('international-destinations/<int:trip_id>/', views.fetch_international_trip_details, name='fetch_domestic_trip_details'),
     path('add_international_trip/', views.add_international_trip, name='add_international_trip'),
+    path('domestic-destinations/<int:trip_id>/update/', views.update_domestic_trip),
     path('international-destinations/<int:trip_id>/delete/', views.delete_international_trip),
 
 ]
