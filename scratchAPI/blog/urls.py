1c6bb1a10f3ef3d025af693c8f2dfebfc6986657
Arpan Shrestha <arpanshrestha12@gmail.com>
2025-08-26 10:53:19 +0545

    Fixed some errors

--- scratchAPI/blog/urls.py
+++ scratchAPI/blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.blogs, name='blogs'),
-    path('<int:blog_id>/', views.fetch_blog_details, name='fetch_blog_details'),
+    # path('<int:blog_id>/', views.fetch_blog_details, name='fetch_blog_details'),
+    path('add_blog/', views.add_blog, name='add_blog'),
+    path('<int:blog_id>/update/', views.update_blog),
+    path('<int:blog_id>/delete/', views.delete_blog),
 ]
